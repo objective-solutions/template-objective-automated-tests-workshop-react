4309850af27433723e261ed6df2eb91b18e40423
Eric Rodrigo de Freitas <eric.freitas@objective.com.br>
2022-07-14 13:09:35 -0300

    [ROC-78] adding a test template for Exercise 3 - Greeting Service

---
+++ src/GreetingService.test.ts
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Exercise 3 - Greeting Service
+ * 
+ * For this exercise we must create a greeting service.
+ *
+ * Given a name when we execute “sayhello” method it should return “Hello name!”
+ * Given a empty input when we execute “sayhello” method it should return “Hello World!”
+ *
+ * Predicted Completion time - 5m
+ *
+ */
+
+import { NOT_IMPLEMENTED_YET } from "./Messages"
+
+describe("GreetingServiceTestSuite", () => {
+  test("Given a name `Otavio` when execute sayHello method then it should return `Hello Otavio!`", () => {
+    throw new Error(NOT_IMPLEMENTED_YET)
+  })
+ 
+  test("Given an empty name `` when execute sayHello method then it should return `Hello World!`", () => {
+    throw new Error(NOT_IMPLEMENTED_YET)
+  }) 
+})
