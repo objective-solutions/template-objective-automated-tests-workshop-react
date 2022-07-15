939a280307873944b658cfa7bb62fec92fecb273
Eric Rodrigo de Freitas <eric.freitas@objective.com.br>
2022-07-14 21:25:18 -0300

    [ROC-78] resolution exercise 1

--- src/OddOrEven.test.ts
+++ src/OddOrEven.test.ts
@@ -10,16 +10,35 @@
 
 import { NOT_IMPLEMENTED_YET } from "./Messages"
 
+
+let isEven = (number:number):boolean => number %  2 === 0;
+let isEvenOrOdd = (number:number):string => {
+  if(!Number.isInteger(number))
+    throw new Error(`Invalid Argument: ${number}`)
+  return isEven(number) ? "even" : "odd"
+}
+
 describe("OddOrEvenTestSuite", () => {
-  test("Given an odd number when execute isEvenOrOdd method then it should return `odd`", () => {
-    throw new Error(NOT_IMPLEMENTED_YET)
+  test.each([
+    [1, false], 
+    [2, true], 
+    [3, false], 
+    [4, true]
+  ])("Given the number %p when execute isEven method then it should return `%p`", (number:number, result: boolean) => {
+    expect(isEven(number)).toBe(result)
   })
 
-  test("Given an even number when execute isEvenOrOdd method then it should return `even`", () => {
-    throw new Error(NOT_IMPLEMENTED_YET)
+  test.each( [
+    {type: "odd", number: 1}, 
+    {type: "even", number: 2}
+  ])("Given an $type number when execute isEvenOrOdd method then it should return `$type`", ({ type, number } ) => {
+    expect(isEvenOrOdd(number)).toBe(type)
   })
 
-  test("Given a non integer number when execute isEvenOrOdd method then it should raise an exception", () => {
-    throw new Error(NOT_IMPLEMENTED_YET)
+  test.each([
+    {number:1.1, error:"Invalid Argument: 1.1"},
+    {number:1.2, error:"Invalid Argument: 1.2"}
+  ])("Given the number $number when execute isEvenOrOdd method then it should raise an exception with error: `$error`", ({number, error}) => {
+    expect(()=> {isEvenOrOdd(number)}).toThrow(error)
   })
 })
